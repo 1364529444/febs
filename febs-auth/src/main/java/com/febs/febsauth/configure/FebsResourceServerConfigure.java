50a45715e6663cabf6688fce3a4bac8c55b3e0d0
13564 <15184482794@qq.com>
2020-03-19 18:37:43 +0800

    init

---
+++ febs-auth/src/main/java/com/febs/febsauth/configure/FebsResourceServerConfigure.java
@@ -0,0 +1,20 @@
+package com.febs.febsauth.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+@Configuration
+@EnableResourceServer
+public class FebsResourceServerConfigure extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .requestMatchers().antMatchers("/**")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated();
+    }
+}
---
+++ febs-common/src/main/java/com/febs/common/entity/FebsAuthException.java
@@ -0,0 +1,9 @@
+package com.febs.common.entity;
+
+public class FebsAuthException extends Exception {
+    private static final long serialVersionUID = -6916154462432027437L;
+
+    public FebsAuthException(String message) {
+        super(message);
+    }
+}
